3ec98e8e16db0ca1382080f92ec550e16604c2b0
Manh692003 <phankhacmanh6903@gmail.com>
2024-09-17 11:23:53 +0700

    test

--- app/src/main/java/com/emanh/mixivivu/view/yacht/YachtFragment.kt
+++ app/src/main/java/com/emanh/mixivivu/view/yacht/YachtFragment.kt
@@ -37,24 +37,4 @@ class YachtFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_yacht, container, false)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment YachtFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            YachtFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
