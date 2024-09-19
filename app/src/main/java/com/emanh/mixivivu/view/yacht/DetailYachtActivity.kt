74f8dbb34ac57501fe2d97d5ce1cdb63af74fb67
nthtnthawngs512 <nthawngs5@gmail.com>
2024-09-19 10:52:46 +0700

    setup layout reviews yacht

--- app/src/main/java/com/emanh/mixivivu/view/yacht/DetailYachtActivity.kt
+++ app/src/main/java/com/emanh/mixivivu/view/yacht/DetailYachtActivity.kt
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.emanh.mixivivu.R
@@ -14,12 +15,16 @@ import com.emanh.mixivivu.databinding.InformationDetailYachtBinding
 import com.emanh.mixivivu.model.InformationYachtModel
 import com.emanh.mixivivu.model.YachtModel
 import com.emanh.mixivivu.view.BaseActivity
+import com.emanh.mixivivu.view.booking.PriceRoomFragment
+import com.emanh.mixivivu.view.reviews.ReviewsFragment
+import com.emanh.mixivivu.viewModel.ReviewsViewModel
 import java.text.NumberFormat
 import java.util.Locale
 
 class DetailYachtActivity : BaseActivity() {
     private lateinit var binding: ActivityDetailYachtBinding
     private lateinit var yachtModel: YachtModel
+    private lateinit var reviewsViewModel: ReviewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +38,12 @@ class DetailYachtActivity : BaseActivity() {
 
     private fun init() {
         yachtModel = intent.getParcelableExtra("objectYacht")!!
+        reviewsViewModel = ViewModelProvider(this)[ReviewsViewModel::class.java]
+        reviewsViewModel.loadReviews(yachtModel.id.toString())
+
+        reviewsViewModel.reviewsSummary.observe(this) {
+            updateReviewInfo(it)
+        }
 
         binding.buttonBack.setOnClickListener { finish() }
 
@@ -40,7 +51,7 @@ class DetailYachtActivity : BaseActivity() {
             binding.characteristic to CharacteristicFragment(),
             binding.priceRoom to PriceRoomFragment(),
             binding.intro to IntroFragment(),
-            binding.evaluation to EvaluationFragment()
+            binding.evaluation to ReviewsFragment()
         )
 
         buttons.forEach { (button, fragment) ->
@@ -116,11 +127,16 @@ class DetailYachtActivity : BaseActivity() {
 
         fragmentTransaction.replace(R.id.frameLayoutDetail, fragment)
         fragmentTransaction.commit()
-        // Kakjshd
     }
 
     private fun formatPrice(price: Int): String {
         val numberFormat = NumberFormat.getNumberInstance(Locale.US)
         return numberFormat.format(price)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateReviewInfo(summary: ReviewsViewModel.ReviewsSummary) {
+        val formattedRating = String.format("%.1f", summary.averageRating)
+        binding.infoEvaluate.text = "$formattedRating (${summary.count} đánh giá)"
     }
 }
